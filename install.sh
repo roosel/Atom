c8ed5165a50c4540b48b9fdc6ac41142dfce9b7c
Brent <puddletowndesign@gmail.com>
2017-04-01 15:03:41 -0600

    updated atom installer script

--- install.sh
+++ install.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+echo "Cloning Atom Configs"
+git clone https://github.com/PuddletownDesign/Atom
 
-cd ~/Config
+echo "Setting up linux configs"
+
+cd Atom/
+git reset --hard origin/linux
+git checkout linux
 ln -s ~/Config/Atom/ ~/.atom
-sudo add-apt-repository ppa:webupd8team/atom -y
-sudo apt update
-sudo apt install atom
+mkdir ../temp
+cd ../temp
+
+echo "Downloading Atom..."
+wget https://github.com/atom/atom/releases/download/v1.15.0/atom-amd64.deb
+
+echo "Installing Atom..."
+sudo dpkg --install atom-amd64.deb
+
+echo "Installing Atom Packages..."
 apm install --packages-file Atom/packages.list
