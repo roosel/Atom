e011b000cb8eaf2277070bdbe67bdc356734c711
Brent <puddletowndesign@gmail.com>
2017-04-01 00:42:37 -0600

    updated atom preferences

--- install.sh
+++ install.sh
@@ -4,5 +4,7 @@ cd ~/Config
 git clone https://github.com/PuddletownDesign/Atom
 git checkout origin/linux
 ln -s ~/Config/Atom/ ~/.atom
-brew cask install atom
+sudo add-apt-repository ppa:webupd8team/atom
+sudo apt update
+sudo apt install atom
 apm install --packages-file Atom/packages.list
