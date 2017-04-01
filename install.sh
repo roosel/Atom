3f571e9ccb1906895b1ef421540d75ffc31d17d2
Brent <puddletowndesign@gmail.com>
2017-04-01 17:17:35 -0600

    updated atom preferences

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-mkdir ~/Documents/Dev/
-cd ~/Documents/Dev/
-git clone https://github.com/PuddletownDesign/Atom
-git checkout origin/mac
+
 ln -s ~/Documents/Dev/Atom ~/.atom
 brew cask install atom
 apm install --packages-file Atom/packages.list
