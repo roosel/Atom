461717e2a3d72e3e9c0f9d4319dfb66dec83eeaf
Brent <puddletowndesign@gmail.com>
2017-04-02 13:57:07 -0500

    updated linux installer

--- install.sh
+++ install.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
+git checkout linux
 
+rm -rf ~/.atom
 ln -s ~/Config/Atom/ ~/.atom
-mkdir ../temp
-cd ../temp
-
-echo "Downloading Atom..."
-wget https://github.com/atom/atom/releases/download/v1.15.0/atom-amd64.deb
-
-echo "Installing Atom..."
-sudo dpkg --install atom-amd64.deb
 
 echo "Installing Atom Packages..."
 apm install --packages-file Atom/packages.list
