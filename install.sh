2c8add21cc162e380a981091b8a2ef9652d91412
Brent <puddletowndesign@gmail.com>
2017-04-02 09:03:45 -0500

    updated linux config

--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-echo "Cloning Atom Configs"
-git clone https://github.com/PuddletownDesign/Atom
 
-echo "Setting up linux configs"
-
-cd Atom/
-git reset --hard origin/linux
-git checkout linux
 ln -s ~/Config/Atom/ ~/.atom
 mkdir ../temp
 cd ../temp
